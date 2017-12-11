9708c9b3812c380c5ce65da6c0de59de1824b7ea
sumit <lord.sbansal@gmail.com>
2017-12-12 00:11:05 +0530

    user_crud_operations

---
+++ depot/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def index
+
+  end
+  def show
+
+  end
+  def new
+
+  end
+  def edit
+
+  end
+  def create
+
+  end
+  def update
+
+  end
+  def distroy
+
+  end
+end
